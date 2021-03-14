3f08a14da6aa651ef3f8761ae836f1a7310616c2
Solidshots <seanbrent5@gmail.com>
2021-03-14 10:40:12 -0400

    Final - Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password-Generator
+
+## Description
+Generate a secure random password based on criteria provided by the user.
+
+## Technologies 
+* HTML
+* CSS
+* JavaScript
+
+## Website URL
+ https://brentsa.github.io/Password-Generator/
+
+## Website Screen
+![Website Screenshot](./assets/images/webpage.png)
+
+## Contribution
+Developed by Sean Brent
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
-// Get references to the #generate element
+//Get references to the #generate element
+//Instantiate global character arrays
 var generateBtn = document.querySelector("#generate");
 var lowercaseArray = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbersArray = "0123456789".split("");
 var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
 
+//Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//Prompt the user for a password length and return the number if valid
 function lengthPrompt()
 {
   var passwordLength = parseInt(prompt("Select length of password between 8 and 128 characters."));
+
+  //Continue prompting the user for a length until it is valid
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
     alert("Please enter a valid number between 8 and 128");
     passwordLength = prompt("Select length of password between 8 and 128 characters.");
   }
+
   return passwordLength;
 }
 
@@ -29,10 +37,12 @@ function randomNumber(min, max)
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//Randomly generate a string depending on the user selected character array and length
 function generateRandomCharacters(characterArray, length)
 {
   var word = "";
 
+  //generate a random character for every character in the string up to our designated lengths
   for(var i = 0; i < length; i++){
     word += characterArray[randomNumber(0, characterArray.length - 1)];
   }
@@ -40,10 +50,12 @@ function generateRandomCharacters(characterArray, length)
   return word;
 }
 
+//Create the pool of characters our generator can draw from based on arguments provided by the user selection
 function createPasswordUserCriteria(length, lower, upper, numbers, special)
 {  
   var randomCharacterArray = [];
 
+  //Depending on the selections from the user we append character arrays into our user defined character array
   if(lower){
     appendCharacters(randomCharacterArray, lowercaseArray);
   }
@@ -57,16 +69,18 @@ function createPasswordUserCriteria(length, lower, upper, numbers, special)
     appendCharacters(randomCharacterArray, specialArray);
   }
 
-  console.log(length);
-  console.log(lower);
-  console.log(upper);
-  console.log(numbers);
-  console.log(special);
-  console.log(randomCharacterArray);
+  //Uncomment code block below to display user inputs in the console log
+  //console.log(length);
+  //console.log(lower);
+  //console.log(upper);
+  //console.log(numbers);
+  //console.log(special);
+  //console.log(randomCharacterArray);
 
   return generateRandomCharacters(randomCharacterArray, length);
 }
 
+//Prompt the user for their password requirement criteria
 function generatePassword()
 {
   //All user selections arise
@@ -94,10 +108,8 @@ function generatePassword()
 function writePassword() 
 {
   var password = generatePassword();
-  
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
